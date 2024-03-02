c95a637847a966604c56459147d839f87bcf3a21
Tushar Dua <Tdua@thesummitgrp.com>
2024-03-02 16:46:05 +0530

    blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(
+        max_length=20,
+        null=True,
+        blank=False
+    )
+    description = models.CharField(max_length=100,null=True,blank=False)
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 # Create your views here.
+
 
 def landing_page(request):
     return render(request, "blog/index.html")
 
+
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    context = {'data':Blog.objects.all()}
+    return render(request, "blog/all-posts.html",context=context)
+
 
 def single_post(request, slug):
-    return render(request, "blog/post-detail.html")
-
+    return render(request, "blog/post-detail.html" )
